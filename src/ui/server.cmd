1a8618010d724e44e1a76f25a1217ecb4e2c5fc1
HazelCross <hazeloliviacross@gmail.com>
2025-05-27 02:02:47 -0400

    release candidate 1

--- src/ui/server.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-START /B node "src\ui\server.js" > "ui.log"
-
-edge "http://localhost:8675/"
-more "ui.log"
--- src/ui/server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-node src/ui/server.js &> ui.log &
-chromium http://localhost:8675/ &> /dev/null &
-tail +0f ui.log
