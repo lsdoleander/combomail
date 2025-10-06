ebeaf747ffc0ab2d75da3ad6f237001fd6dd5927
LSD.oleander <lsdoleander@gmail.com>
2025-10-06 07:30:51 -0400

    fix

--- src/modules/@proxy.js
+++ src/modules/@proxy.js
@@ -1,9 +1,7 @@
 
-import decodo from "../conf/decodo.js"
-import floxy from "../conf/floxy.js"
 import nord from "../conf/nord.js"
 
-let proxy, proxyqueue = proxy = [ ...nord, ...decodo, ...floxy ];
+let proxy, proxyqueue = proxy = [ ...nord ];
 
 export function nextproxy() {
 	if (proxyqueue.length === 0) proxyqueue = proxy;
