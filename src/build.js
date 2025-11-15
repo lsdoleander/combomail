6179c37c30244a55a96d01fd709a2281a2b3a0ce
LSD.oleander <lsdoleander@gmail.com>
2025-11-15 15:47:39 -0500

    deps fixed

--- src/build.js
+++ src/build.js
@@ -6,6 +6,7 @@
 
 	if (fs.existsSync(path.resolve("gen"))) fs.rmSync(path.resolve("gen"), { recursive: true });
 	if (fs.existsSync(path.resolve("dist"))) fs.rmSync(path.resolve("dist"), { recursive: true });
+	if (fs.existsSync(path.resolve("node_modules"))) fs.rmSync(path.resolve("node_modules"), { recursive: true });
 
 	fs.mkdirSync(path.resolve("gen/linux"), { recursive: true });
 	fs.mkdirSync(path.resolve("gen/win"), { recursive: true });
